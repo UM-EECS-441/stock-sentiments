fb84fe3de50b85e1d0690bc37e1e6f7f1e95eca6
Jacob Hodgson <jhodg@umich.edu>
2020-10-10 18:54:08 +0000

    Adding backend to one folder

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/stocksentiments/iosdevs/env"
+set -gx VIRTUAL_ENV "/root/stocksentiments/iosdevs/backend/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
