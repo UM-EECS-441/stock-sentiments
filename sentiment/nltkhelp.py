449067a0c5b24fd8cd4871e0fc955e42a4b46924
nairv <nairv@umich.edu>
2020-10-21 16:13:22 -0400

    helpful nltk pip install msg

--- sentiment/nltkhelp.py
+++ sentiment/nltkhelp.py
@@ -1,6 +1,6 @@
 import nltk
 import ssl
-
+# had to run /Application/Python\ 3.7/Install\ commands.command
 try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
