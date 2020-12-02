50abb1b9a52344ef9f44f3e34c415330dfcdcaaa
pranays <pranays@umich.edu>
2020-12-01 21:56:03 -0500

    Fixed app logo resolution in LaunchScreenVC and MainVC. Added app icon and app name.

--- frontend/stockSentiments/stockSentiments/Controllers/ViewControllers/MainVC.swift
+++ frontend/stockSentiments/stockSentiments/Controllers/ViewControllers/MainVC.swift
@@ -16,16 +16,12 @@ class MainVC: UIViewController/*, ReturnDelegate */{
 
     @IBOutlet weak var signinButton: CustomButton!
     @IBOutlet weak var appTitleLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         signinButton.tintColor = .systemBlue
         appTitleLabel.textColor = .label
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         appTitleLabel.center.x = view.center.x // Place it in the center x of the view.
         appTitleLabel.center.x -= view.bounds.width // Place it on the left of the view with the width = the bounds'width of the view.
         // animate it from the left to the right
